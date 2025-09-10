35b66d6ba4fc37240e070abcae114d9e009961ef
Kaio Muniz <kkaioribeiro@gmail.com>
2025-09-10 12:01:18 -0300

    commit com apenas o docker-compose com banco postgre para fazer o exercicio no pgadmin

---
+++ src/main/java/br/com/kaiomuniz/ProjetoExercicioBancoApplication.java
@@ -0,0 +1,13 @@
+package br.com.kaiomuniz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoExercicioBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoExercicioBancoApplication.class, args);
+	}
+
+}
